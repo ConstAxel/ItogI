fc39dcd77c23941f5218aeb7f8017403951acf9d
gosha_mineev@outlook.com <gosha_mineev@outlook.com>
2022-03-02 13:13:25 +0300

    Добавьте файлы проекта.

---
+++ Itog/Itog.cs
@@ -0,0 +1,55 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itog
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class NumberRooms : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            FilteredElementCollector roomList = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Rooms);
+            IList<ElementId> roomids = roomList.ToElementIds() as IList<ElementId>;
+
+            List<ViewPlan> listView = new FilteredElementCollector(doc)
+                .OfClass(typeof(ViewPlan))
+                .OfType<ViewPlan>()
+                .ToList();
+            Transaction transaction = new Transaction(doc, "Создание нумерации помещения");
+            transaction.Start();
+            foreach (View view in listView)
+            {
+                foreach (ElementId roomid in roomids)
+                {
+                    Element element = doc.GetElement(roomid);
+                    Room room = element as Room;
+                    XYZ roomCenter = GetElementCenter(room);
+                    UV center = new UV(roomCenter.X, roomCenter.Y);
+                    doc.Create.NewRoomTag(new LinkElementId(roomid), center, view.Id);
+                }
+            }
+            transaction.Commit();
+            return Result.Succeeded;
+        }
+
+        public XYZ GetElementCenter(Element element)
+        {
+            BoundingBoxXYZ bounding = element.get_BoundingBox(null);
+            return (bounding.Max + bounding.Min) / 2;
+        }
+    }
+}
